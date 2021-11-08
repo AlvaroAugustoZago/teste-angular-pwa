2a9e50bd660fbe0619493143b2ce813c8f51fdcd
Álvaro Zago <alvaro.zago@wtti.com.br>
2021-11-08 15:36:01 -0300

    🐛 fix: Adicionado para registrar depois de estabilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ const initializer = (pwaService: PwaService) => () => pwaService.initPwaPrompt()
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
-      //registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   entryComponents: [
