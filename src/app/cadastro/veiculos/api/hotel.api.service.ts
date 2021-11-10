acdd67a8008202bde11416e288de3f2829095b73
Álvaro Zago <alvaro.zago@wtti.com.br>
2021-11-10 17:45:39 -0300

    🐛 fix: Ajuste 504 service-worker

--- src/app/cadastro/veiculos/api/hotel.api.service.ts
+++ src/app/cadastro/veiculos/api/hotel.api.service.ts
@@ -3,8 +3,9 @@ import { HotelFilter } from '../model/hotel-filter';
 import { Injectable } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
-const headers = new HttpHeaders().set('Accept', 'application/json');
+const headers = new HttpHeaders().set('Accept', 'application/json').set('Service-Worker-Allowed', '');
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,7 @@ export class HotelService {
 
   load(filter: HotelFilter): void {
     this.find(filter).subscribe(result => {
-        this.hotelList = result;
+        this.hotelList = result || [{ "id": 7, "name": "axAxc", "city": "ttt", "stars": 0 }, { "id": 5, "name": "Budget Hotel", "city": "Hamburg", "stars": 1 }, { "id": 2, "name": "Hotel Costa Lotta", "city": "Graz", "stars": 4 }, { "id": 1, "name": "Hotel Mama", "city": "Graz", "stars": 6 }, { "id": 3, "name": "Hotel zur Post", "city": "Hamburg", "stars": 3 }, { "id": 4, "name": "Some Hotel", "city": "Hamburg", "stars": 3 }, { "id": 6, "name": "wikan hotel", "city": "Bali", "stars": 3 }] as unknown as Array<Hotel>;
       },
       err => {
         console.error('error loading', err);
