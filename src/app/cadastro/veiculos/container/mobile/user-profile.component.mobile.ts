d88500dfea1d77edf8ddbdd5f24ad64618e171e7
Álvaro Zago <alvaro.zago@wtti.com.br>
2021-11-16 11:24:49 -0300

    ✨ feature: Alterado para vizualização de arquivos

--- src/app/cadastro/veiculos/container/mobile/user-profile.component.mobile.ts
+++ src/app/cadastro/veiculos/container/mobile/user-profile.component.mobile.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApplicationStateService } from 'src/app/application-state.service';
 import { HotelService } from '../../api/hotel.api.service';
 import { Hotel } from '../../model/hotel';
@@ -13,14 +14,47 @@ import { UserProfileComponent } from '../user-profile.component';
 })
 export class UserProfileComponentMobile extends UserProfileComponent {
 
-  constructor(applicationStateService: ApplicationStateService, private hotelService: HotelService) {
+  // constructor(applicationStateService: ApplicationStateService, private hotelService: HotelService) {
+  //   super(applicationStateService);
+  //   this.hotelService.load(new HotelFilter());
+  // }
+
+  // get hotelList(): Hotel[] {
+  //   return this.hotelService.hotelList;
+  // }
+
+
+  filePath: string;
+  myForm: FormGroup;
+
+  constructor(applicationStateService: ApplicationStateService, public fb: FormBuilder) {
     super(applicationStateService);
-    this.hotelService.load(new HotelFilter());
+    this.myForm = this.fb.group({
+      img: [null],
+      filename: ['']
+    })
   }
 
-  get hotelList(): Hotel[] {
-    return this.hotelService.hotelList;
+  ngOnInit(): void { }
+
+  imagePreview(e) {
+    const file = (e.target as HTMLInputElement).files[0];
+
+    this.myForm.patchValue({
+      img: file
+    });
+
+    this.myForm.get('img').updateValueAndValidity()
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.filePath = reader.result as string;
+    }
+    reader.readAsDataURL(file)
   }
 
+  submit() {
+    console.log(this.myForm.value)
+  }
 
 }
